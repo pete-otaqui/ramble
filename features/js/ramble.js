f2db6d06e2e1221d36e1672f3a48d5b010e767f6
Pete Otaqui <pete.otaqui@bbc.co.uk>
2010-07-05 19:50:23 +0800

    refactoring for multiple classes and dependency injection pretty well under way.  Need to implement "visit" and actual tests.

--- features/js/ramble.js
+++ features/js/ramble.js
@@ -1,3 +1,274 @@
+/**
+ * Cucumber Test Runner and Gherkin Parser
+ * @author Jamie Hill <jamie@soniciq.com>
+ * @author Pete Otaqui <pete@otaqui.com>
+ * @version $Rev$
+ * @requires jQuery
+ */
+var Ramble = {
+    debug : true,
+    _debug : function() {
+        console.log(arguments);
+    }
+}
+
+/**
+ * Gherkin DSL parser
+ * @version $Rev$
+ * @requires Ramble
+ */
+
+Ramble.Parser = {
+    parseFeatureFile : function(data) {
+        var feature = new Ramble.Feature();
+        var describingFeature = true;
+        var lines = data.split('\n');
+        var line, scenario;
+        feature.title = lines.splice(0,1)[0];
+        $.each(lines,function() {
+            line = $.trim(this.toString());
+            if ( describingFeature ) {
+                // Really need to make a proper parser! Still, got a class for it now.
+                if ( line != "" ) {
+                    feature.description += line + "\n";
+                } else {
+                    describingFeature = false;
+                    return;
+                }
+            } else {
+                if ( this.indexOf('Scenario:') != -1 ) {
+                    scenario = new Ramble.Scenario();
+                    feature.scenarios.push(scenario);
+                    scenario.title = line;
+                } else {
+                    scenario.steps.push(line);
+                }
+            }
+        });
+        return feature;
+    },
+    getExampleCode : function(step) {
+        var code = step.replace(/^(Given|When|Then|And)\s+/, '').replace(/"([^\"]*)"/, '"([^\"]*)"');
+        var args = [];
+        var quotes = step.match(/"/g);
+        if(quotes) {
+          for(i = 0; i < parseInt(quotes.length / 2); i++) {
+            args.push('arg' + (i + 1));
+          }
+        }
+        args = args.join(', ');
+        var example = "Missing step definition:<br/>";
+        example += '<pre><code>';
+        example += 'ramble.match(/^' + code + '$/, function(' + args + ') {\n  // code\n});';
+        example += '</code></pre>';
+        return example;
+    }
+};
+Ramble.Feature = function() {
+    this.title = '';
+    this.description = '';
+    this.scenarios = [];
+}
+Ramble.Scenario = function() {
+    this.title = '';
+    this.steps = [];
+};
+/**
+ * Outputter Interface defines the methods all outputters should have.
+ */
+Ramble.IOutputter = {
+    start : function() {},
+    outputFeature : function ( feature ) {},
+    ouputScenario : function ( scenario ) {},
+    outputStep : function ( step ) {},
+    stop : function() {}
+};
+Ramble.HtmlOutputter = {
+    _currentFeature : null,
+    _currentScenario : null,
+    _currentSteps : null,
+    results : null,
+    start : function() {
+        this.results = $(this.results_selector);
+    },
+    outputFeature : function ( feature ) {
+        var div = $('<div/>', {class:'ramble-feature'});
+        this.results.append(div);
+        div.append($('<h3/>', {text:feature.title}));
+        var description = feature.description.split('\n').join('<br/>');
+        div.append($('<p/>', {class:'ramble-description',html:description}));
+        this._currentFeature = div;
+    },
+    outputScenario : function ( scenario ) {
+        var div = $('<div/>', {class:'ramble-scenario'});
+        this._currentFeature.append(div);
+        div.append($('<h4/>', {text:scenario.title}));
+        this._currentScenario = div;
+        this._currentSteps = $('<ul/>', {class:'ramble-steps'});
+        div.append(this._currentSteps);
+    },
+    outputStep : function ( step, status ) {
+        var className = 'ramble-'+status;
+        var li = this._currentSteps.append($('<li/>', {class:className, html:step}));
+    },
+    stop : function() {},
+    results_selector : '#results'
+};
+
+
+/**
+ * Loads and runs Gherkin feature files against Step matcher files
+ * @author Pete Otaqui <pete@otaqui.com>
+ * @version $Rev$
+ */
+Ramble.Runner =  {
+    iframe : null,
+    workspace_selector : '#workspace',
+    outputter : Ramble.HtmlOutputter,
+    parser : Ramble.Parser,
+    features : [],
+    matchers : [],
+    paths : [],
+    init : function() {
+        this.outputter.start();
+        if ( !this.iframe ) {
+            this.iframe = $('<iframe id="browser" />').appendTo(this.workspace_selector);
+        }
+    },
+    /**
+     * Loads a feature file using Ajax
+     * @public
+     * @param String file URL to load
+     * @param Bool run_now run the file immediately after loading?
+     * @returns void
+     */
+    loadFeatureFile : function(file, run_now) {
+        Ramble._debug('loadFeatureFile', file, run_now);
+        this._files[file] = run_now;
+        $.ajax({
+            url: file,
+            success: function(data) {
+                Ramble.Runner._parseFeatureFile(data, file);
+            },
+            dataType: 'text/plain',
+            async: false
+        });
+    },
+    /**
+     * Shortcut to loadFeatureFile(file, true)
+     * @see loadFeatureFile
+     * @public
+     * @param String file PAth to the feature file
+     * @returns void
+     */
+    run : function(file) {
+        this.loadFeatureFile(file, true);
+    },
+    /**
+     * Add a matcher
+     * @public
+     * @param RegExp match The regular expression match
+     * @param Function test The test to run on the page
+     * @return void
+     */
+    match : function(regexp, test) {
+        var matcher = {regexp:regexp, test:test};
+        Ramble._debug("add matcher", matcher);
+        this.matchers.push(matcher);
+    },
+    /**
+     * Runs an array of features, defaults to own features array if
+     * one is not provided.
+     * @public
+     * @param Array features OPTIONAL array of features
+     * @returns void
+     */
+    runFeatures : function(features) {
+        if ( !features ) features = this.features;
+        Ramble._debug("Running features: ", features);
+        for ( var i=0, ii=feature.length; i<ii; i++ ) {
+            this.runFeature(features[i]);
+        }
+    },
+    /**
+     * Runs a feature.
+     * @public
+     * @returns void
+     */
+    runFeature : function(feature) {
+        Ramble._debug("Running feature: ", feature);
+        this.outputter.outputFeature(feature);
+        var scenarios = feature.scenarios;
+        for ( var i=0, ii=feature.scenarios.length; i<ii; i++ ) {
+            this.runScenario(scenarios[i]);
+        }
+    },
+    /**
+     * Runs a scenario.
+     * @public
+     * @param Ramble.Scenario scenario The scenario to run
+     * @returns void
+     */
+    runScenario : function(scenario) {
+        Ramble._debug("Running scenario: ", scenario);
+        this.outputter.outputScenario(scenario);
+        for ( var i=0, ii=scenario.steps.length; i<ii; i++ ) {
+            this.runStep(scenario.steps[i]);
+        }
+    },
+    /**
+     * Runs a step.
+     * @public
+     * @param string step The step to run
+     * @returns void
+     */
+    runStep : function(step) {
+        Ramble._debug("Running step: ", step);
+        var status = "",
+            found = null;
+        if ( step.indexOf('#') == 0 ) {
+            status = "comment";
+        } else {
+            $.each(this.matchers, function() {
+                var match = step.replace(/^(Given|When|Then|And)\s+/, '').match(this.regexp);
+                if ( match ) {
+                    found = { matches: match.slice(1), func: this.func };
+                    return;
+                }
+            })
+        }
+        if ( found !== null ) {
+            try {
+                var result = found.func.apply(elements)
+            } catch (error) {
+                
+            }
+        } else {
+            status = "missing";
+            step = Ramble.Parser.getExampleCode(step);
+        }
+        this.outputter.outputStep(step, status);
+    },
+    /**
+     * Adds a path matcher
+     * @public
+     * @param RegExp regexp The "name" of the page as a regex
+     * @param String path The url of the page
+     * @returns void
+     */
+    addPath : function(regexp, path) {
+        this.paths.push({regexp:regexp, path:path});
+    },
+    _files : {},
+    _parseFeatureFile : function(data, file) {
+        var feature = Ramble.Parser.parseFeatureFile(data);
+        this.features.push( feature );
+        if ( this._files[file] ) this.runFeature(feature);
+    }
+}
+var ramble = Ramble.Runner;
+
+/*
 var ramble = {
   debug: false,
   iframe: null,
@@ -131,3 +402,5 @@ var ramble = {
     };
   }
 };
+
+*/
