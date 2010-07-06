eb6457b609660130d9754b59ad6a9eaa59eedfe1
Pete Otaqui <pete.otaqui@bbc.co.uk>
2010-07-06 23:12:31 +0100

    add speed modes and timeout mechanism to drive them

--- features/js/ramble.js
+++ features/js/ramble.js
@@ -187,7 +187,11 @@ Ramble.Runner =  {
     features : [],
     matchers : [],
     page_loading : false,
-    init : function() {
+    options : {
+        speed : "fast"
+    },
+    init : function(options) {
+        this.options = $.extend(this.options, options);
         this.outputter.start();
         if ( !this.iframe ) {
             this.iframe = $('<iframe id="browser" />').appendTo(this.workspace_selector);
@@ -235,7 +239,20 @@ Ramble.Runner =  {
         while ( this._queue_index < this._queue.length-1 ) {
             if ( Ramble.Runner.page_loading ) {
                 return;
+            } else if ( Ramble.Runner.options.speed != "fast" ) {
+                var date = new Date();
+                var time = date.getTime();
+                clearTimeout(Ramble.Runner._time_out);
+                Ramble.Runner._time_out = setTimeout(function() {
+                    Ramble.Runner.run(elements);
+                }, 100);
+                if ( time < Ramble.Runner._time_next ) {
+                    return;
+                }
+                Ramble.Runner._time_next = time + Ramble.Runner._times[ Ramble.Runner.options.speed ];
             }
+            
+            
             var item = this._queue[ this._queue_index ];
             var found;
             $.each(this._befores, function() {
@@ -346,143 +363,10 @@ Ramble.Runner =  {
     },
     _befores : [],
     _queue : [],
-    _queue_index : 0
+    _queue_index : 0,
+    _times : {
+        "medium" : 700,
+        "slow"   : 2000
+    }
 }
 var ramble = Ramble.Runner;
-
-/*
-var ramble = {
-  debug: false,
-  iframe: null,
-  results: null,
-  pageLoading: false,
-  steps: [],
-  matchers: [],
-  paths: [],
-  init: function(workspace_selector, results_selector) {
-    this.iframe = $('<iframe id="browser" />').appendTo(workspace_selector);
-    this.results = $(results_selector);
-
-    this.iframe.css('width', 500).css('height', 300).load(function() {
-      ramble.pageLoading = false;
-      var contents = $(this).contents();
-      // Not entirely sure why we need to add click event here really.
-      contents.find('a').click(function() {
-        ramble.getUrl($(this).attr('href'));
-      });
-      // Need a better solution for handling loading in general.
-      contents.find('form').submit(function() {
-        ramble.pageLoading = true;
-      });
-      ramble._run(contents);
-    }).bind('urlChange.ramble', function(event, data) {
-      ramble.pageLoading = true;
-      $(this).attr('src', data.href);
-    });
-  },
-  run: function(path) {
-    $.ajax({
-      url: path,
-      success: function(data) { 
-        ramble._parse(data);
-        ramble._run();
-      },
-      dataType: 'text/plain',
-      async: false
-    });
-  },
-  addPath: function(regexp, path) {
-    if(typeof(path) != 'string' && typeof(path) != 'function') throw('Must supply string or function for path');
-    this.paths.push({ regexp: regexp, path: path });
-  },
-  pathTo: function(path) {
-    found = null;
-    $.each(this.paths, function() {
-      var match = path.match(this.regexp);
-      if(match) {
-        found = { matches: match.slice(1), path: this.path };
-        return;
-      };
-    });
-    
-    if(found == null) {
-      this.results.append($('<p/>', { text: "Couldn't find path for: " + path, 'class': 'error' }));
-    } else {
-      try {
-        return typeof(found.path) == 'string' ? found.path : found.path.apply(ramble, found.matches);
-      } catch(error) {
-        this.results.append($('<p/>', { html: 'Problem with path: ' + path + '<br/> - ' + error, 'class': 'error' }));
-      }
-    }
-  },
-  getUrl: function(url) {
-    this.iframe.trigger('urlChange.ramble', { href: url });
-  },
-  match: function(regexp, func) {
-    this.matchers.push({ regexp: regexp, func: func });
-  },
-  _parse: function(data) {
-    $.each(data.split('\n'),function() {
-      var trimmed = $.trim(this.toString());
-      if(trimmed != '') ramble.steps.push(trimmed);
-    });
-  },
-  _run: function(elements) {
-    var matchers = this.matchers;
-    
-    while(this.steps.length > 0) {
-      // If the page is loading we can return as the load event will re-trigger
-      // this method when the new page has finished loading.
-      if(ramble.pageLoading) return;
-      
-      if(ramble.debug) alert('Press OK to continue');
-      
-      var line = this.steps.shift().toString();
-      // These checks are all a little crude but until we need proper parser this works.
-      if(line.indexOf('Feature:') == 0) {
-        this.results.append($('<h3/>', { text: line.replace(/^Feature:\s+/, '') }));
-        continue;
-      } else if(line.indexOf('Scenario:') == 0) {
-        this.results.append($('<p/>', { text: line, 'class': 'scenario' }));
-        continue;
-      } else if (line.indexOf('#') == 0) {
-        this.results.append($('<p/>', { text: line, 'class': 'comment' }));
-        continue;
-      }
-      
-      var found = null;
-      $.each(matchers, function() {
-        var match = line.replace(/^(Given|When|Then|And)\s+/, '').match(this.regexp);
-        if(match) {
-          found = { matches: match.slice(1), func: this.func };
-          return;
-        };
-      });
-      
-      if(found == null) {
-        var code = line.replace(/^(Given|When|Then|And)\s+/, '').replace(/"([^\"]*)"/, '"([^\"]*)"');
-        var args = [];
-        var quotes = line.match(/"/g);
-        if(quotes) {
-          for(i = 0; i < parseInt(quotes.length / 2); i++) {
-            args.push('arg' + (i + 1));
-          }
-        }
-        
-        this.results.append($('<p/>', { 
-          html: "Missing step definition:<br/><pre>ramble.match(/^" + code
-                          + '$/, function(' + args.join(', ') + ') {\n  // code\n});</pre>', 'class': 'missing'
-        }));
-      } else {
-        try {
-          var result = found.func.apply(elements, found.matches);
-          this.results.append($('<p/>', { text: line, 'class': 'ok' }));
-        } catch(error) {
-          this.results.append($('<p/>', { html: line + '<br/> - ' + error, 'class': 'error' }));
-        }
-      }
-    };
-  }
-};
-
-*/
